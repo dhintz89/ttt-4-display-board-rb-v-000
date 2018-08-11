d7e497fa71bb484b0ac0be06c24bf01f82449eba
Dan Hintz <dhintz@live.com>
2018-08-11 01:34:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  
+  puts" #{board[0]} | #{board[1]} | #{board[2]} "
+  separator = "-----------"
+  puts row,separator,row,separator,row
+end
